a5a22dbe4a0f95b94716e68d9e5f71da9304335b
bourneymart91 <bourneymart91@googlemail.com>
2016-04-19 10:46:46 +0800

    New File : GetGeometricMeanFromPrevious
    Added new file which can compute the geometric mean of a partition of
    the Sylvester matrix given the previous geometric mean, reducing
    computational complexity.

---
+++ GetGeometricMeanFromPrevious.m
@@ -0,0 +1,23 @@
+function [GM] = GetGeometricMeanFromPrevious(fx,GM_prev,m,n_k)
+
+
+
+p1 = (m+n_k+1) ./ (n_k+1)
+p1 = p1 .^((n_k+2)./(n_k+1))
+
+p2 = GM_prev .^((n_k+2)./(n_k+1));
+
+p3 = nchoosek(m+n_k,m) .^ (1./(n_k+1))
+
+prod_a = 1;
+prod_b = 1;
+for i = 0:1:m
+    prod_a = prod_a * abs(fx(i+1))
+    prod_b = prod_b * nchoosek(n_k+1+i,i)
+end
+
+p4 = (1./(prod_a * prod_b * prod_b)) .^(1./((n_k+1)*(m+1)))
+
+GM = p1 * p2 * p3 * p4
+
+end
